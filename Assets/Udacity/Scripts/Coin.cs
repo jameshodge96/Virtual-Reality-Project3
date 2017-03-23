590a217f8d2e480069e35ebd20ee02dce41931e7
James Hodge <jameshodge96@users.noreply.github.com>
2017-03-23 12:24:52 +0000

    feat: added coinPoof for coin to have animation
    Added the coinPoof animation to the coin where its located. This then animates once clicked. The coin will then be destroyed.

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+    public GameObject CoinProofPrefab;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+        Vector3 positionOfCoin = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.transform.position.z);
+		Instantiate(CoinProofPrefab, positionOfCoin, Quaternion.Euler(-90, 0, 0));
+		Destroy(gameObject);
     }
 
 }
