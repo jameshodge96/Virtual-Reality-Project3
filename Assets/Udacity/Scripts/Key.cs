4a518567872656bfefc49563305b3adc6934949e
James Hodge <jameshodge96@users.noreply.github.com>
2017-03-23 16:09:54 +0000

    feat: added key animation and key unlock
    Key continues to rotate until it has been clicked on. upon being clicked, the poofprefab is instantiated causing the gameObject to be destroyed along with calling the Unlock function in Door script. This results in the users ability to open the door.

--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Key : MonoBehaviour 
 {
     //Create a reference to the KeyPoofPrefab and Door
+    public GameObject KeyPoofPrefab;
+    public Door door;
 
 	void Update()
 	{
 		//Not required, but for fun why not try adding a Key Floating Animation here :)
+		transform.Rotate (0, 50.0f * Time.deltaTime, 0);
 	}
 
 	public void OnKeyClicked()
@@ -18,6 +21,13 @@ public class Key : MonoBehaviour
         // Call the Unlock() method on the Door
         // Set the Key Collected Variable to true
         // Destroy the key. Check the Unity documentation on how to use Destroy
+
+		Vector3 positionOfKey = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.transform.position.z);
+		Instantiate(KeyPoofPrefab, positionOfKey, Quaternion.Euler(-90, 0, 0));
+		door.Unlock();
+		Destroy(gameObject);
+
+
     }
 
 }
