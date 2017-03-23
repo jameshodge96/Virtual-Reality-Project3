2d6ee731afb746e06c63160b63a626fe3c7f7992
James Hodge <jameshodge96@users.noreply.github.com>
2017-03-23 16:05:53 +0000

    feat: added lock and unlock to the door
    Once the key has been clicked on, the Unlock will be called creating the locked boolean to go to false. In doing so, when the player clicks on the Door it will open up causing the door to rotate up allowing the player into the temple. If the key has not been clicked on, then the door will play the locked door sound and not open.

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,20 +7,54 @@ public class Door : MonoBehaviour
     // Create a boolean value called "locked" that can be checked in OnDoorClicked() 
     // Create a boolean value called "opening" that can be checked in Update() 
 
+    public bool locked = true;
+    public bool opening = false;
+    public bool fullRasied = true;
+
+    public AudioClip lockedDoor;
+    public AudioClip openDoor;
+
+
     void Update() {
-        // If the door is opening and it is not fully raised
-            // Animate the door raising up
+		// If the door is opening and it is not fully raised
+        // Animate the door raising up
+
+
+    	if (opening == true){
+			if (gameObject.transform.position.y <= 24) {
+				gameObject.transform.Translate (0, 10f * Time.deltaTime, 0);
+				}
+			}       
     }
 
     public void OnDoorClicked() {
+
+		AudioSource doorAudio = gameObject.GetComponent<AudioSource> ();
+
         // If the door is clicked and unlocked
             // Set the "opening" boolean to true
         // (optionally) Else
             // Play a sound to indicate the door is locked
+
+            if(locked == false){
+            opening = true;
+            doorAudio.clip = openDoor;
+            doorAudio.Play();
+
+           } else {
+            	doorAudio.clip = lockedDoor;
+            	doorAudio.Play();
+
+            }
+
     }
 
     public void Unlock()
     {
         // You'll need to set "locked" to false here
+        locked = false;
     }
 }
+
+
+
